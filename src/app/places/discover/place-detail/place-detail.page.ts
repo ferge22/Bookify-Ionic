5486ccc168bd9d3a3f41f053b677fdafa820af6e
ferge22 <rytisspitrys@gmail.com>
2019-03-29 16:08:27 +0200

    navigating back with navctrl

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
   templateUrl: './place-detail.page.html',
   styleUrls: ['./place-detail.page.scss'],
 })
+
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private navCrtl: NavController) { }
 
   ngOnInit() {
+  }
+
+  onBookPlace() {
+    // this.router.navigate(['/places/tabs/discover']);
+    this.navCrtl.navigateBack('/places/tabs/discover');
   }
 
 }
