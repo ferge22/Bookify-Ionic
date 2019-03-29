539e7c99afe40a862006c8e3ed622ee17f8e8949
ferge22 <rytisspitrys@gmail.com>
2019-03-29 15:31:54 +0200

    outputting places

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from 'src/app/services/places.service';
+import { Place } from '../places.model';
 
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
   styleUrls: ['./discover.page.scss'],
 })
-export class DiscoverPage implements OnInit {
 
-  constructor() { }
+export class DiscoverPage implements OnInit {
+  loadedPlaces: Place[];
+
+  constructor(private placeService: PlacesService) { }
 
   ngOnInit() {
+    this.loadedPlaces = this.placeService.places;
   }
 
 }
---
+++ src/app/services/places.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Place } from '../places/places.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place('p1',
+    'Manhattan Mansion',
+    'In the heart of New York City', 
+    'https://upload.wikimedia.org/wikipedia/commons/e/e8/Hamilton_MT_Daly_Mansion_2.jpg',
+     499.99
+    ),
+     new Place('p2',
+     'L\'Amour Toujours',
+     'A romantic place in Paris',
+     'https://s-ec.bstatic.com/images/hotel/max1024x768/120/120685028.jpg',
+      189.99
+    ),
+    new Place('p3',
+    'The Foggy Palace',
+    'Not your average city trip!',
+    'https://c1.staticflickr.com/8/7149/6593601281_a6e79d1252_b.jpg',
+     99.99
+   ),
+
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+
+  constructor() { }
+}
