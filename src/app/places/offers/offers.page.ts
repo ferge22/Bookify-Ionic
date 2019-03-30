b6ead89128e0fa1fc86fc948f4404e04c95d880e
ferge22 <rytisspitrys@gmail.com>
2019-03-30 16:55:19 +0200

    swipeable list items in offer page

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from 'src/app/services/places.service';
 import { Place } from '../places.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
   styleUrls: ['./offers.page.scss'],
 })
+
 export class OffersPage implements OnInit {
   loadedOffers: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
     this.loadedOffers = this.placesService.places;
   }
 
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+    console.log('Editing item!', offerId);
+  }
 }
