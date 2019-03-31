1be69c162c187f87bc519b96bca3ad5acb46413d
ferge22 <rytisspitrys@gmail.com>
2019-03-31 17:52:13 +0300

    adding template driven form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -26,8 +27,12 @@ export class AuthPage implements OnInit {
       setTimeout(() => {
         loadingEl.dismiss();
         this.router.navigateByUrl('/places/tabs/discover');
-      }, 2000);
+      }, 1500);
     });
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 
 }
